3a15942778e7d981e9daadab9fcf3ad73b248c89
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-26 19:07:23 -0400

    Project init

---
+++ src/main/java/com/example/SpringBootRestApiWithJpaRepo/SpringBootRestApiWithJpaRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestApiWithJpaRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiWithJpaRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiWithJpaRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootRestApiWithJpaRepo/SpringBootRestApiWithJpaRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestApiWithJpaRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiWithJpaRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
